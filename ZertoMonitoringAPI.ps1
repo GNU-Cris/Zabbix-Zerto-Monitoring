23dafb0bd80d977774ee1a7703a6d34ac297af3a
GNU-Cris <marcelocristofani@gmail.com>
2024-06-14 16:13:37 -0300

    Update ZertoMonitoringAPI.ps1

--- ZertoMonitoringAPI.ps1
+++ ZertoMonitoringAPI.ps1
@@ -1,118 +1,128 @@
-$strZVMIP = "IPADDRES_OR_FQDN"
-$strZVMPort = "PORT" # Usually TCP:9669
-$userName = "USERNAME" # vCenter Username
-$password = "PASSWORD" # vCenter password
+$strZVMIP = "10.213.17.13"
+$strZVMPort = "9669" # Usually TCP:9669
+$username = "marcelo.santos@br.clara.net" # Zerto username
+$password = "W12kstd%" # Zerto password
 
 # Allow self-signed certificates
 [System.Net.ServicePointManager]::ServerCertificateValidationCallback = {$true}
 $progressPreference = "silentlyContinue"
 
-# Authentication - Get a session key
-$baseURL = "https://" + $strZVMIP + ":"+$strZVMPort
-$xZertoSessionURI = $baseURL +"/v1/session/add"
-$authInfo = ("{0}:{1}" -f $userName,$password)
-$authInfo = [System.Text.Encoding]::UTF8.GetBytes($authInfo)
-$authInfo = [System.Convert]::ToBase64String($authInfo)
-$headers = @{Authorization=("Basic {0}" -f $authInfo)}
-$body = '{"AuthenticationMethod": "1"}'
-$contentType = "application/json"
-$xZertoSessionResponse = Invoke-WebRequest -Uri $xZertoSessionURI -Headers $headers -Method POST -Body $body -ContentType $contentType 
-$zertSessionHeader = $xZertoSessionResponse.headers.get_item("x-zerto-session")
+# Authentication - Get a session token
+$authURL = "https://analytics.api.zerto.com/v2/auth/token"
+$authBody = @{
+    username = $username
+    password = $password
+} | ConvertTo-Json -Compress
+
+$authHeaders = @{
+    "accept" = "application/json"
+    "Content-Type" = "application/json"
+}
+
+try {
+    $authResponse = Invoke-RestMethod -Uri $authURL -Method Post -Body $authBody -Headers $authHeaders
+    $token = $authResponse.token
+} catch {
+    Write-Error "Failed to obtain authentication token. Error: $_"
+    exit
+}
+
+# Ensure the token was obtained
+if (-not $token) {
+    Write-Error "Failed to obtain authentication token."
+    exit
+}
+
+$headers = @{
+    "Authorization" = "Bearer $token"
+}
 
 $ITEM = [string]$args[0]
 $VpgIdentifier = [string]$args[1]
 
 switch ($ITEM) {
-	"Discovery" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$VPGs = $data.VPGIdentifier 
-		#Count the VPGs
-		$count = $VPGs | Measure-Object
-		$count = $VPGs.count
-		#Open the JSON output
-		$JSON = '{"data": [{ '
-		# Add the various elements to the JSON
-		foreach ($VpgIdentifier in $VPGs) {
-			$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-			$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-			$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-			$JSON = $JSON + '"{#VPGIDENTIFIER}": "' + $VpgIdentifier + '",'
-			$JSON = $JSON + '"{#VPGNAME}": "' + $data.VPGName + '"'
-			if ($count -eq 1) {
-				# Close JSON
-				$JSON = $JSON + '}]}'
-			} else {
-				$JSON = $JSON + '},{'
-				$count--
-			}
-		}
-		#Output JSON
-		$JSON
-	}
-	"VPGName" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.VPGName
-	}
-	"SourceSite" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.SourceSite
-	}
-	"TargetSite" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.TargetSite
-	}
-	"UsedStorageInMB" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data = $data.UsedStorageInMB 
-		$data = $Data *= 1000000 # From MB bytes
-		$data
-	}
-	"VmsCount" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.VmsCount
-	}
-	"LastTest" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		#Convert to epoch time for Zabbix
-		$unixEpochStart = new-object DateTime 1970,1,1,0,0,0,([DateTimeKind]::Utc)
-		[int]($data.LastTest - $unixEpochStart).TotalSeconds
-	}
-	"ActualRPO" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.ActualRPO
-	}
-	"Status" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.Status
-	}
-	"SubStatus" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}  
-		$data.SubStatus
-	}
-	"RawVPGData" {
-		$vpgListApiUrl = "https://" + $strZVMIP + ":"+$strZVMPort+"/v1/vpgs"
-		$data = Invoke-RestMethod -Uri $vpgListApiUrl -Header @{ "x-zerto-session" = $zertSessionHeader }
-		$data
-	}
+    "Discovery" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $VPGs = $data.VPGIdentifier
+        $count = $VPGs.count
+        $JSON = '{"data": [{ '
+
+        foreach ($VpgIdentifier in $VPGs) {
+            $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+            $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+            $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+            $JSON += '"{#VPGIDENTIFIER}": "' + $VpgIdentifier + '",'
+            $JSON += '"{#VPGNAME}": "' + $data.VPGName + '"'
+            if ($count -eq 1) {
+                $JSON += '}]}'
+            } else {
+                $JSON += '},{'
+                $count--
+            }
+        }
+        $JSON
+    }
+    "VPGName" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.VPGName
+    }
+    "SourceSite" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.SourceSite
+    }
+    "TargetSite" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.TargetSite
+    }
+    "UsedStorageInMB" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data = $data.UsedStorageInMB
+        $data *= 1000000
+        $data
+    }
+    "VmsCount" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.VmsCount
+    }
+    "LastTest" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $unixEpochStart = [DateTime]::new(1970,1,1,0,0,0, [DateTimeKind]::Utc)
+        [int]($data.LastTest - $unixEpochStart).TotalSeconds
+    }
+    "ActualRPO" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.ActualRPO
+    }
+    "Status" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.Status
+    }
+    "SubStatus" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data = $data | where {$_.VpgIdentifier -eq $VpgIdentifier}
+        $data.SubStatus
+    }
+    "RawVPGData" {
+        $vpgListApiUrl = "https://$strZVMIP:$strZVMPort/v1/vpgs"
+        $data = Invoke-RestMethod -Uri $vpgListApiUrl -Headers $headers
+        $data
+    }
 }
-
-
